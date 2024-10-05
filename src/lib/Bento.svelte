89b865be71d464d3c19b0f8307e88cb1b9bdb116
Jazza231 <greeny-231@outlook.com>
2024-10-05 14:52:21 +0800

    Add bentos

---
+++ src/lib/Bento.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+   const props = $props();
+   console.log(props);
+   const { header, imagePath, short, content, href } = props;
+
+   const bentoClass = header.toLowerCase().replaceAll(" ", "-");
+</script>
+
+<div class={`bento bento-${bentoClass}`} class:short>
+   <div class="text">
+      <h3 class="header"><a {href}>{header}</a></h3>
+      <p>{content}</p>
+   </div>
+   <a {href} aria-label="Link to {header} project page.">
+      <img src="/images/{imagePath}" alt="Image for {header}" />
+   </a>
+</div>
+
+<style lang="scss">
+   .bento {
+      display: flex;
+      flex-direction: column;
+      padding: 2rem;
+      border-radius: 0.5rem;
+      backdrop-filter: blur(0.1rem);
+      gap: 2rem;
+      border: var(--secondary-400) solid 0.2rem;
+      box-shadow: var(--secondary-200) 0 0 7rem;
+      margin: 2rem 3rem 4rem 0;
+
+      .text {
+         display: flex;
+         flex-direction: column;
+      }
+
+      .header {
+         font-size: 1.5rem;
+         margin-bottom: 0.5rem;
+         min-height: 2rem;
+
+         a {
+            color: var(--primary);
+            transition:
+               color 200ms,
+               font-size 200ms;
+            font-size: 1.5rem;
+
+            &:hover {
+               color: var(--text-800);
+               font-size: 1.6rem;
+            }
+         }
+      }
+
+      img {
+         width: auto;
+         height: 100%;
+         border-radius: 0.5rem;
+         box-shadow: 0 0 0.5rem var(--primary);
+         transition: all 0.2s ease-in-out;
+
+         &:hover {
+            transform: scale(1.05);
+         }
+      }
+
+      &.short img {
+         width: 100%;
+         height: auto;
+         position: relative;
+      }
+
+      width: 40rem;
+      height: 20rem;
+
+      &.short {
+         width: 20rem;
+      }
+
+      &:not(.short) {
+         flex-direction: row;
+      }
+   }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,71 @@
 <script lang="ts">
+   import Bento from "$lib/Bento.svelte";
    const { data } = $props();
    const { age } = data;
 </script>
 
-<div class="greeting">
-   <div class="glow"></div>
-   <div class="hi">
-      Hi! I'm <span class="colour-effect">Jazza</span>
+<div class="home">
+   <div class="greeting">
+      <div class="glow"></div>
+      <div class="hi">
+         Hi! I'm <span class="colour-effect">Jazza</span>
+      </div>
+   </div>
+
+   <div class="who">
+      <h2>Who am I?</h2>
+      <p>
+         My name is Jazza, and I am an {age} year old beginner programmer. I began
+         to learn programming about {age - 17 === 1
+            ? "1 year"
+            : age - 17 + " years"} ago, starting with JS in order to contribute to
+         the Scratch Addons project.
+      </p>
+      <p>
+         My relationship with programming is one of immense love-filled hatred.
+         <br />
+         Looking at you CSS. (SASS makes it bearable.)
+      </p>
+      <p>
+         I currently attend a TAFE institution in Western Australia for a music
+         technology course, another of my passions. When I'm not studying there,
+         I am coding. I am still 100% a beginner, and am currently learning
+         svelte+kit. When I am not studying or coding, I am probably gaming. I
+         play relatively popular games, such as Red Dead Redemption 2, CS2,
+         Forza Horizon 5, and Cyberpunk 2077. You can check out screenshots <a
+            href="/screenshots">here</a
+         >
+         and my rig <a href="/rig">here.</a>
+      </p>
+   </div>
+
+   <div class="projects">
+      <h2>Projects</h2>
+      <div class="bentos">
+         <Bento
+            header="Scratch Addons"
+            imagePath="Scratch Addons.png"
+            content="A collection of addons for the kids' coding website; Scratch."
+            href="/projects/scratch-addons"
+         />
+         <Bento
+            header="MediaScout"
+            imagePath="MediaScout.png"
+            content="A movie explorer and player designed to just be...better"
+            href="/projects/mediascout"
+            short
+         />
+      </div>
    </div>
 </div>
 
-<div class="who">
-   <h2>Who am I?</h2>
-   <p>
-      My name is Jazza, and I am an {age} year old beginner programmer. I began to
-      learn programming about {age - 17 === 1 ? "1 year" : age - 17 + " years"} ago,
-      starting with JS in order to contribute to the Scratch Addons project.
-   </p>
-   <p>
-      My relationship with programming is one of immense love-filled hatred.
-      <br />
-      Looking at you CSS. (SASS makes it bearable.)
-   </p>
-   <p>
-      I currently attend a TAFE institution in Western Australia for a music
-      technology course, another of my passions. When I'm not studying there, I
-      am coding. I am still 100% a beginner, and am currently learning
-      svelte+kit. When I am not studying or coding, I am probably gaming. I play
-      relatively popular games, such as Red Dead Redemption 2, CS2, Forza
-      Horizon 5, and Cyberpunk 2077. You can check out screenshots <a
-         href="/screenshots">here</a
-      >
-      and my rig <a href="/rig">here.</a>
-   </p>
-</div>
-
 <style lang="scss">
+   .home {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 3rem;
+   }
    .greeting {
       display: flex;
       justify-content: center;
@@ -44,6 +74,11 @@
       .hi {
          font-size: 4rem;
       }
+   }
+
+   .bentos {
+      display: flex;
+      flex-direction: row;
    }
 
    .glow {
