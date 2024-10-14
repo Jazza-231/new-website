e29fb1ba7f4b96b66f1cdf9feb0bcb55aed52c01
Jazza <greeny-231@outlook.com>
2024-10-14 14:51:00 +0800

    Start of projects

---
+++ src/routes/projects/[project]/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+   const { data } = $props();
+   const { slug } = data;
+</script>
+
+<h1>{slug}</h1>
---
+++ src/routes/projects/[project]/+page.ts
@@ -0,0 +1,7 @@
+import type { PageLoad } from "./$types";
+
+export const load = (async ({params}) => {
+   return {
+    slug: params.project
+   };
+}) satisfies PageLoad;
