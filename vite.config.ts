4a18ec3e5978361c2d6bcc2213ae375e0db7bcbe
Jazza231 <greeny-231@outlook.com>
2024-11-05 13:14:38 +0800

    A lot of changes, i fucked up but oh well

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import { enhancedImages } from "@sveltejs/enhanced-img";
-import imageProcessingPlugin from "./image-processor";
 
 export default defineConfig({
-   plugins: [enhancedImages(), sveltekit(), imageProcessingPlugin()],
+   plugins: [enhancedImages(), sveltekit()],
    test: {
       include: ["src/**/*.{test,spec}.{js,ts}"],
    },
